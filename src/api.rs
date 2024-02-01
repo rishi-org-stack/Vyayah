146edf05e678b972943f0ea24a7b0465f5d2c90a
rishi_muni <rishi.jha_xt@angkas.com>
2024-02-01 22:36:19 +0530

    added api and custom handler

--- src/api.rs
+++ src/api.rs
@@ -1,37 +1,53 @@
-use std::env;
-use std::error::Error;
+use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
-use crate::protocol::request::Function;
+pub struct API {
+    // logger
+    // handler: Arc<dyn HandlerFn + Sync>,
+    addr: String,
+}
 
-pub async fn api() -> Result<(), Box<dyn Error>> {
-    let addr = env::args()
-        .nth(1)
-        .unwrap_or_else(|| "127.0.0.1:8080".to_string());
+trait HandlerFn: Send + Sync {
+    fn handle(&self, input: Vec<u8>) -> Vec<u8>;
+}
 
-    let listener = TcpListener::bind(&addr).await?;
-    println!("Listening on: {}", addr);
+impl API {
+    pub fn new(host: String) -> API {
+        API {
+            addr: format!("0.0.0.0:{}", host),
+            // handler: Arc::new(handler),
+        }
+    }
+    pub async fn run<'a, F>(self, op: F) -> Result<(), Box<dyn std::error::Error>>
+    where
+        F: Fn(Vec<u8>) -> Vec<u8> + Sync + Send + 'static,
+    {
+        let listener = TcpListener::bind(&self.addr).await?;
+        // use logger here
+        println!("Listening on: {}", self.addr);
+        let op = Arc::new(op);
 
-    loop {
-        let (mut socket, _) = listener.accept().await?;
-        tokio::spawn(async move {
-            let mut buf = vec![0; 1024];
-            loop {
-                let n = socket
-                    .read(&mut buf)
-                    .await
-                    .expect("failed to read data from socket");
+        loop {
+            let op = op.clone();
+            let (mut socket, _) = listener.accept().await?;
+            tokio::spawn(async move {
+                let mut input = vec![0; 1024];
+                loop {
+                    let n = socket.read(&mut input).await.expect("unhandled: read");
 
-                if n == 0 {
-                    return;
+                    if n == 0 {
+                        return;
+                    }
+
+                    let result_buff = op(input.clone());
+
+                    socket
+                        .write_all(result_buff.as_slice())
+                        .await
+                        .expect("unhandled: failed to write data to socket");
                 }
-                Function::parse(&buf);
-                socket
-                    .write_all(&buf[0..n])
-                    .await
-                    .expect("failed to write data to socket");
-            }
-        });
+            });
+        }
     }
 }
